f070377df073b81b0dc17cc7fa1d34c9059ce80c
alexandrakomkova <sashakomkova2001@gmail.com>
2021-11-01 10:23:23 +0300

    parse data, write it to file

---
+++ task1.rb
@@ -0,0 +1,71 @@
+require 'csv'
+require 'rubygems'
+require 'curb'
+require 'nokogiri'
+
+
+def get_html(url)
+  http = Curl.get(url) do |curl|
+    curl.ssl_verify_peer=false
+    curl.ssl_verify_host=0
+  end
+  Nokogiri::HTML(http.body_str)
+end
+
+def parse_product(url)
+
+  html = get_html(url)
+  product_name= html.xpath('//h1[@class="product_main_name"]').text
+  product_img=html.xpath('//*[@id="bigpic"]/@src')
+  product_weight_variation=html.xpath('//*[@class="radio_label"]')
+  price_per_weight=html.xpath('//*[@class="price_comb"]')
+
+  (0...product_weight_variation.length).each do |variation_counter|
+    data_to_write =[product_name, product_img,  product_weight_variation[variation_counter].text.to_s, price_per_weight[variation_counter].text.to_s]
+    puts product_name, product_img,  product_weight_variation[variation_counter].text.to_s, price_per_weight[variation_counter].text.to_s
+    write_to_file("parsingProducts.csv", data_to_write, product_name)
+  end
+
+end
+def parse_one_page(count_products, url)
+  product_page = get_html(url).xpath('//div[@class="product-desc display_sd"]/a/@href')
+
+  (0...count_products).each do |product_counter|
+    parse_product(product_page[product_counter].to_s.gsub(/\s+/, ""))
+  end
+end
+
+def parse(url, file_name)
+  puts "-----start parsing-----\n\n"
+  CSV.open(file_name, 'w')
+  count_products=get_html(url).xpath('//input[@id="nb_item_bottom"]/@value').text.to_i
+  product_per_page = 25
+  count_pages = (count_products/product_per_page.to_f).ceil
+  (1..count_pages).each do |p_counter|
+    url=url+"?p="+"#{p_counter}" if p_counter>1
+    if(count_products<product_per_page)
+      parse_one_page(count_products, url)
+    else
+      parse_one_page(product_per_page, url)
+    end
+    count_products-=product_per_page
+  end
+  puts "-----finished parsing-----"
+end
+
+def write_to_file(file_name, data_to_write, product_name)
+  CSV.open(file_name, 'a+') do |row|
+    row << data_to_write
+  end
+  puts "-----product #{product_name} is written-----\n\n"
+end
+
+
+
+parse('https://www.petsonic.com/farmacia-para-gatos/', "parsingProducts.csv")
+
+
+
+
+
+
